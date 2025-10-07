a7237a9ab9c739bf8e9dca32e6b51b63721902f0
SoraOrihara <caca66z@gmail.com>
2025-10-07 15:16:11 -0300

    Criação dos componentes na tela e criação da pagina sobre e linkando ela

---
+++ src/main/java/br/com/springEstudo/SpringTmdbGui/gui/Sobre.java
@@ -0,0 +1,98 @@
+package br.com.springEstudo.SpringTmdbGui.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Desktop;
+
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.SystemColor;
+import java.awt.event.ActionListener;
+import java.net.URI;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Cursor;
+
+public class Sobre extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sobre dialog = new Sobre();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Sobre() {
+		setModal(true);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/br/com/springEstudo/SpringTmdbGui/gui/img/home20x20.png")));
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Buscador de filmes Tmdb 1.0");
+		lblNewLabel.setBounds(10, 11, 232, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Author: Caio de Oliveira");
+		lblNewLabel_1.setBounds(10, 71, 115, 14);
+		getContentPane().add(lblNewLabel_1);
+		
+		JButton btnGithub = new JButton("");
+		btnGithub.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnGithub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				link("https://github.com/SoraOrihara/ExercicioGUIcomSpringEApiTMDB");
+			}
+		});
+		btnGithub.setIcon(new ImageIcon(Sobre.class.getResource("/br/com/springEstudo/SpringTmdbGui/gui/img/github20x20.png")));
+		btnGithub.setBounds(23, 160, 89, 23);
+		getContentPane().add(btnGithub);
+		
+		JLabel lblNewLabel_2 = new JLabel("Api: ");
+		lblNewLabel_2.setBounds(10, 114, 46, 14);
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("https://developer.themoviedb.org/docs/getting-started");
+		lblNewLabel_3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblNewLabel_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				link("https://developer.themoviedb.org/docs/getting-started");
+			}
+		});
+		lblNewLabel_3.setForeground(SystemColor.textHighlight);
+		lblNewLabel_3.setBounds(55, 114, 324, 14);
+		getContentPane().add(lblNewLabel_3);
+
+	}
+
+	private void link(String site) {
+		Desktop desktop = Desktop.getDesktop();
+		try {
+			URI uri = new URI(site);
+			desktop.browse(uri);
+		}catch(Exception e) {
+			System.out.print(e);
+		}
+	}
+	
+}
--- src/main/java/br/com/springEstudo/SpringTmdbGui/gui/TelaDadosCinema.java
+++ src/main/java/br/com/springEstudo/SpringTmdbGui/gui/TelaDadosCinema.java
@@ -12,18 +12,30 @@ import javax.swing.border.EmptyBorder;
 import org.springframework.stereotype.Component;
 
 import br.com.springEstudo.SpringTmdbGui.business.FilmeService;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.Cursor;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 @Component
 public class TelaDadosCinema extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private final FilmeService filmeService;
+	private JTextField txtNomeFilme;
 
 
 	/**
 	 * Create the frame.
 	 */
 	public TelaDadosCinema(FilmeService filmeService) {
+		setResizable(false);
 		this.filmeService=filmeService;
 		setIconImage(Toolkit.getDefaultToolkit().getImage(TelaDadosCinema.class.getResource("/br/com/springEstudo/SpringTmdbGui/gui/img/home20x20.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,7 +43,55 @@ public class TelaDadosCinema extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNomeFilme = new JLabel("Nome do filme:");
+		lblNomeFilme.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNomeFilme.setBounds(10, 32, 96, 14);
+		contentPane.add(lblNomeFilme);
+		
+		txtNomeFilme = new JTextField();
+		txtNomeFilme.setText("");
+		txtNomeFilme.setBounds(116, 30, 217, 20);
+		contentPane.add(txtNomeFilme);
+		txtNomeFilme.setColumns(10);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(343, 29, 89, 23);
+		contentPane.add(btnBuscar);
+		
+		JLabel lblTituloFilme = new JLabel("Titulo do Filme");
+		lblTituloFilme.setBounds(41, 91, 143, 20);
+		contentPane.add(lblTituloFilme);
+		
+		JLabel lblNota = new JLabel("Nota:");
+		lblNota.setBounds(41, 130, 143, 14);
+		contentPane.add(lblNota);
+		
+		JTextArea txtAreaSinopse = new JTextArea();
+		txtAreaSinopse.setText("Sinopse");
+		txtAreaSinopse.setEditable(false);
+		txtAreaSinopse.setLineWrap(true);
+		txtAreaSinopse.setWrapStyleWord(true);
+		txtAreaSinopse.setBounds(41, 155, 211, 241);
+		contentPane.add(txtAreaSinopse);
+		
+		JLabel lblPoster = new JLabel("");
+		lblPoster.setBounds(299, 91, 200, 311);
+		contentPane.add(lblPoster);
+		
+		JButton btnSobre = new JButton("");
+		btnSobre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Sobre sobre = new Sobre();
+				sobre.setVisible(true);
+			}
+		});
+		btnSobre.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnSobre.setBackground(SystemColor.control);
+		btnSobre.setIcon(new ImageIcon(TelaDadosCinema.class.getResource("/br/com/springEstudo/SpringTmdbGui/gui/img/about20x20.png")));
+		btnSobre.setBounds(489, 30, 20, 20);
+		contentPane.add(btnSobre);
 
 	}
-
 }
