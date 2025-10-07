48316c4e38f4173679681e0b996c6b706fe2f95d
SoraOrihara <caca66z@gmail.com>
2025-10-07 14:41:52 -0300

    Criação da gui e conectando ao spring

--- src/main/java/br/com/springEstudo/SpringTmdbGui/EstudoGuiComSpringBootETmdbApplication.java
+++ src/main/java/br/com/springEstudo/SpringTmdbGui/EstudoGuiComSpringBootETmdbApplication.java
@@ -1,13 +1,32 @@
 package br.com.springEstudo.SpringTmdbGui;
 
-import org.springframework.boot.SpringApplication;
+import java.awt.EventQueue;
+
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import br.com.springEstudo.SpringTmdbGui.gui.TelaDadosCinema;
 
 @SpringBootApplication
+@ComponentScan(basePackages="br.com.springEstudo.SpringTmdbGui")
 public class EstudoGuiComSpringBootETmdbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EstudoGuiComSpringBootETmdbApplication.class, args);
+		 // Inicia o contexto do Spring de uma forma que não presume um ambiente web (headless(false))
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(EstudoGuiComSpringBootETmdbApplication.class)
+        		 .web(WebApplicationType.NONE)
+                .headless(false)
+                .run(args);
+
+        // Pede para a fila de eventos do Swing para criar e mostrar a nossa GUI
+        EventQueue.invokeLater(() -> {
+            // Pega o nosso JFrame do contexto do Spring (que já tem o serviço injetado)
+            TelaDadosCinema ex = context.getBean(TelaDadosCinema.class);
+            ex.setVisible(true);
+        });
 	}
 
 }
